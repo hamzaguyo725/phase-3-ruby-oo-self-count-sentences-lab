48a904ab57a5ba04f8cb60afd911b801e06b76a4
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-06-01 13:14:44 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    puts self.split(/\.|\?|!/).filter{|sentence| !sentence.empty?}.size
   end
 end
+"Hello! How was ur day. Will u go?".count_sentences
+#binding.pry
